271a5de77a43ee6380fafd00105c423de3830a35
TangxzNLP <tangxunzhu@163.com>
2019-11-04 22:00:33 +0800

    dataset

---
+++ Txz_RNN/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 21:53:00 2019
+
+@author: daniel
+"""
+
+from .rnn import rnn_cell, rnn_forward
---
+++ Txz_RNN/rnn.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 21:51:30 2019
+
+@author: daniel
+"""
+import torch
+
+def rnn_cell(xt, a_prev, parameters):
+        """
+    Implements a single forward step of the RNN-cell as described in Figure (2)
+
+    Arguments:
+    xt -- your input data at timestep "t", numpy array of shape (n_x, m).
+    a_prev -- Hidden state at timestep "t-1", numpy array of shape (n_a, m)
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        ba --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    Returns:
+    a_next -- next hidden state, of shape (n_a, m)
+    yt_pred -- prediction at timestep "t", numpy array of shape (n_y, m)
+    cache -- tuple of values needed for the backward pass, contains (a_next, a_prev, xt, parameters)
+    """
+    
+    # Retrieve parameters from "parameters"
+    Wax = parameters["Wax"]
+    Waa = parameters["Waa"]
+    Wya = parameters["Wya"]
+    ba = parameters["ba"]
+    by = parameters["by"]
+    ### START CODE HERE ### (≈2 lines)
+    # compute next activation state using the formula given above
+    a_next = np.tanh(np.dot(Wax, xt) + np.dot(Waa, a_prev) + ba)
+    # compute output of the current cell using the formula given above
+    yt_pred = softmax(np.dot(Wya, a_next) + by)   
+    ### END CODE HERE ###
+    
+    # store values you need for backward propagation in cache
+    cache = (a_next, a_prev, xt, parameters)
+    
+    return a_next, yt_pred, cache
+
+def rnn_forward(x, a0, parameters):
+    """
+    Implement the forward propagation of the recurrent neural network described in Figure (3).
+
+    Arguments:
+    x -- 每一个时间步的输入数据，shape为(n_x, m, T_x)
+    a0 -- 初始隐含状态，shape (n_a, m)
+    parameters -- python字典包含:
+                        Waa -- 权重矩阵，用来乘以隐含状态， numpy array数据类型，shape (n_a, n_a) 
+                        Wax -- 权重矩阵，用来乘以输入值， numpy array数据类型， shape (n_a, n_x)
+                        Wya -- 权重矩阵，用来做输出层前与隐含层计算的， numpy array数据类型， shape (n_y, n_a)
+                        ba --  Bias numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+
+    Returns:
+    a -- 每一个时间步的隐含状态, numpy array of shape (n_a, m, T_x)
+    y_pred -- 每一个时间步的预测值, numpy array of shape (n_y, m, T_x)
+    caches -- tuple of values needed for the backward pass, contains (list of caches, x)
+    """
+    
+    # Initialize "caches" which will contain the list of all caches
+    caches = []
+    
+    # Retrieve dimensions from shapes of x and parameters["Wya"]
+    n_x, m, T_x = x.shape
+    n_y, n_a = parameters["Wya"].shape
+    
+    ### START CODE HERE ###
+    
+    # initialize "a" and "y" with zeros (≈2 lines)
+    a = np.zeros((n_a, m, T_x))
+    y_pred = np.zeros((n_y, m, T_x)) #2*10*4
+    
+    # Initialize a_next (≈1 line)
+    a_next = a0
+    
+    # loop over all time-steps
+    for t in range(T_x):
+        # Update next hidden state, compute the prediction, get the cache (≈1 line)
+        a_next, yt_pred, cache = rnn_cell_forward(x[:,:,t], a_next, parameters)
+        # Save the value of the new "next" hidden state in a (≈1 line)
+        a[:,:,t] = a_next
+        # Save the value of the prediction in y (≈1 line)
+        y_pred[:,:,t] = yt_pred
+        # Append "cache" to "caches" (≈1 line)
+        caches.append(cache)
+        
+    ### END CODE HERE ###
+    
+    # store values needed for backward propagation in cache
+    caches = (caches, x)
+    
+    return a, y_pred, caches
